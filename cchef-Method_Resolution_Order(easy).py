314b13bf305e70f4924725cdbd608f85a3a0d4d6
rohegde7 <hegde.rohit7@gmail.com>
2018-01-10 23:44:44 +0530

    ET 0.05 secs

---
+++ cchef-Method_Resolution_Order(easy).py
@@ -0,0 +1,19 @@
+# Author: https://www.github.com/rohegde7 (Rohit Hegde - hegde.rohit7@gmail.com)
+
+# https://www.codechef.com/problems/MRO
+# Problem Code: MRO
+# status: coding
+
+tests = int(input())
+
+for i in range(tests):
+    n = int(input())  #number of songs
+    list_songs = list(map(int, input().split()))
+    k = int(input())
+
+    uncle_johny = list_songs[k-1]   #cause the question's indexing starts from 1
+
+    list_songs.sort()
+
+    print(list_songs.index(uncle_johny)+1)
+
